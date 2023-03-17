228fd1bc9aa06aa782956035b803e4585a87cf0a
eamondang <eamondang@gmail.com>
2023-03-17 15:59:37 +0700

    876 - Ransom Note

---
+++ src/easy/e383_ransom_note.rs
@@ -0,0 +1,52 @@
+#![allow(dead_code)]
+struct Solution;
+use std::collections::HashMap;
+
+impl Solution {
+    fn can_construct(ransom_note: String, magazine: String) -> bool {
+        let mut char_freq: HashMap<char, i32> = HashMap::new();
+
+        // Count the frequency of each character in the magazine
+        for c in magazine.chars() {
+            *char_freq.entry(c).or_insert(0) += 1;
+        }
+
+        // Check if each character in the ransom note can be constructed
+        for c in ransom_note.chars() {
+            if let Some(freq) = char_freq.get_mut(&c) {
+                if *freq > 0 {
+                    *freq -= 1;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn chars_3() {
+        assert!(Solution::can_construct("aa".to_string(), "aab".to_string()));
+    }
+
+
+    #[test]
+    #[should_panic]
+    fn chars_1() {
+        assert!(Solution::can_construct("a".to_string(), "b".to_string()));
+    }
+
+    #[test]
+    #[should_panic]
+    fn chars_2() {
+        assert!(Solution::can_construct("aa".to_string(), "ab".to_string()));
+    }
+}
